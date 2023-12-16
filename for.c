6279dc8485f95bf910ed0d963ef88fd09e30a00d
ioannco <cheremisenov96@gmail.com>
2023-12-16 15:20:44 +0000

    build script

--- for.c
+++ for.c
@@ -17,28 +17,28 @@
 #ifdef MINI_DATASET
 #define N (2*2*2*2*2 + 2)
 #ifndef CHECKSUM
-#define CHECKSUM 2723874.750000
+#define CHECKSUM 130680.328125
 #endif
 #endif
 
 #ifdef SMALL_DATASET
 #define N (2*2*2*2*2*2 + 2)
 #ifndef CHECKSUM
-#define CHECKSUM 54117800.000000
+#define CHECKSUM 2723874.750000
 #endif
 #endif
 
 #ifdef MEDIUM_DATASET
 #define N (2*2*2*2*2*2*2 + 2)
 #ifndef CHECKSUM
-#define CHECKSUM 953108160.000000
+#define CHECKSUM 54117800.000000
 #endif
 #endif
 
 #ifdef LARGE_DATASET
 #define  N  (2*2*2*2*2*2*2*2 + 2)
 #ifndef CHECKSUM
-#define CHECKSUM 12370568192.000000
+#define CHECKSUM 953108160.000000
 #endif
 #endif
 
