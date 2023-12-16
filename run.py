39deee10f58e13a0d9d49c1883e62966566c4f15
ioannco <cheremisenov96@gmail.com>
2023-12-16 16:44:33 +0000

    Add x86_data

--- run.py
+++ run.py
@@ -10,17 +10,14 @@ def compile_and_run(source: Path, output: Path, compile_flags: list):
     flag_string = ' '.join(f'-{f}' for f in compile_flags if f is not None)
 
     compile_string = f'gcc {source} {flag_string} -o {build_dir/source.stem}'
-    print('+ ' + compile_string)
     os.system(compile_string)
     run_string = f'{build_dir/source.stem} >> {output}'
-    print('+ ' + run_string)
     os.system(run_string)
 
 
 if __name__ == '__main__':
     # Инициализируем директорию сборки
     os.mkdir(build_dir)
-
 
     num_threads = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 40, 60, 80, 100, 120, 140, 160]
     dataset_sizes = ['MINI_DATASET', 'SMALL_DATASET', 'MEDIUM_DATASET', 'LARGE_DATASET']
@@ -33,6 +30,7 @@ if __name__ == '__main__':
         for dataset in dataset_sizes:
             os.system(f"echo -n '{dataset},' >> {output_file}")
             for threads in num_threads:
+                print(f'Optimization: {optimization:<15} Dataset: {dataset:<15} Threads: {threads:<15}')
                 compile_and_run(source, output_file, ['fopenmp', optimization, f'DNUM_THREADS={threads}', f'D{dataset}'])
                 os.system(f"echo -n  ',' >> {output_file}")
             os.system(f"echo '' >> {output_file}")
