798c6c49c1a6f0d12032688836939df58d949a71
ioannco <cheremisenov96@gmail.com>
2023-12-16 17:01:47 +0000

    added config

--- optimized.c
+++ optimized.c
@@ -5,40 +5,9 @@
 #include <omp.h>
 #include <assert.h>
 #include <sys/time.h>
+#include "config.h"
 
 #define  Max(a, b) ((a)>(b)?(a):(b))
-
-#if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
-#define MINI_DATASET
-#endif
-
-#ifdef MINI_DATASET
-#define N (2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 2723874.750000
-#endif
-#endif
-
-#ifdef SMALL_DATASET
-#define N (2*2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 54117800.000000
-#endif
-#endif
-
-#ifdef MEDIUM_DATASET
-#define N (2*2*2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 953108160.000000
-#endif
-#endif
-
-#ifdef LARGE_DATASET
-#define  N  (2*2*2*2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 12370568192.000000
-#endif
-#endif
 
 float maxeps = 0.1e-7;
 int itmax = 100;
--- task.c
+++ task.c
@@ -4,44 +4,9 @@
 #include <time.h>
 #include <omp.h>
 #include <sys/time.h>
+#include "config.h"
 
 #define  Max(a, b) ((a)>(b)?(a):(b))
-
-#if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(MEDIUM_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
-#define MINI_DATASET
-#endif
-
-#ifdef MINI_DATASET
-#define N (2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 2723874.750000
-#endif
-#endif
-
-#ifdef SMALL_DATASET
-#define N (2*2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 54117800.000000
-#endif
-#endif
-
-#ifdef MEDIUM_DATASET
-#define N (2*2*2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 953108160.000000
-#endif
-#endif
-
-#ifdef LARGE_DATASET
-#define  N  (2*2*2*2*2*2*2*2*2 + 2)
-#ifndef CHECKSUM
-#define CHECKSUM 12370568192.000000
-#endif
-#endif
-
-#ifndef NUM_THREADS
-#define NUM_THREADS 8
-#endif
 
 const float NNN3 = 1.f / (N * N * N);
 
